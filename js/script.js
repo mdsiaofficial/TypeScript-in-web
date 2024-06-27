f0318f6c6722926b0f01a6625edebe2ea1d6036e
mdsiaofficial <mdsiaofficial@gmail.com>
2024-06-27 23:21:04 +0600

    "Added new JavaScript and TypeScript code for functions, variables, and data types, including function declarations, array and object manipulations, and type annotations."

--- js/script.js
+++ js/script.js
@@ -13,4 +13,91 @@ console.log(fvck);
 fvck = "nigga";
 fvck = 56;
 console.log(fvck);
-fu;
+function gunkoro(a, b) {
+    return a * b;
+}
+console.log(gunkoro(6, 7));
+console.log(gunkoro(6, ""));
+console.log(gunkoro(6, 4));
+let fru = ['apel', 'kola', 'jambura', 'lebu', 45, true];
+fru.push(53);
+console.log(fru);
+let mixed = ['kola', 120, true,];
+mixed.push('nashpati', 'kathal', 400);
+console.log(mixed);
+mixed.push({
+    name: 'Ashiq',
+});
+console.log(mixed);
+let person = {
+    name: 'Ashiq',
+    age: 24,
+    isCap: true,
+};
+// person.country = "bd";
+person['country'] = "bd";
+console.log(person);
+let a;
+a = "sna";
+let b;
+b = 'sddf';
+let ab;
+ab = ['sdf', 'sdf', 'sdf'];
+console.log(ab);
+ab.push('sdf');
+console.log(ab);
+let jakhusi = [];
+jakhusi.push('ter');
+console.log(jakhusi);
+let arekta;
+let bb;
+bb = {
+    na: "ashiq",
+    kam: "thinking",
+};
+console.log(bb);
+let mm = {};
+mm.boyosd = 43;
+mm.namd = "ashiq";
+console.log(mm);
+let h = {};
+h.name = "dslkf";
+let u;
+u = 45;
+u = "sdlfj";
+let rt = [];
+rt.push("sdflk");
+rt.push(45);
+console.log(rt);
+const myF = () => {
+    console.log("first");
+};
+let my;
+my = 5;
+const myFuc = (a, b) => {
+    console.log(a, b);
+};
+myFuc("hello", 56);
+const hidfuc = (a, b, c) => {
+    c ? console.log(c) : null;
+    console.log(a + b);
+    return a + b + c;
+};
+hidfuc("helo", "hi", 67);
+hidfuc("helo", "hi");
+function ff(a, b) {
+    return 5;
+}
+console.log("first");
+const userDetails = (id, user) => {
+    return id;
+};
+const uDetails = (user) => {
+    console.log(`Hello ${user.name}, ${user.age > 40 ? "sir" : "mr "}`);
+};
+const user1 = {
+    id: 1,
+    name: 'Ashiq',
+    age: 24
+};
+console.log(uDetails(user1));
--- ts/script.ts
+++ ts/script.ts
@@ -19,4 +19,152 @@ fvck = 56;
 
 console.log(fvck)
 
-fu
+function gunkoro (a:number, b:number): number {
+  return a * b;
+}
+
+console.log(gunkoro(6, 7));
+console.log(gunkoro(6, ""));
+console.log(gunkoro(6, 4));
+
+let fru = ['apel', 'kola', 'jambura', 'lebu', 45, true];
+fru.push(53);
+console.log(fru);
+
+let mixed = ['kola', 120, true, ];
+mixed.push('nashpati', 'kathal', 400);
+console.log(mixed);
+mixed.push({
+  name: 'Ashiq',
+});
+console.log(mixed);
+
+let person = {
+  name: 'Ashiq',
+  age: 24,
+  isCap: true,
+}
+
+// person.country = "bd";
+person['country'] = "bd";
+console.log(person)
+
+let a: string;
+
+a = "sna";
+let b: number;
+
+b = 'sddf';
+
+let ab: string[];
+ab = ['sdf', 'sdf', 'sdf'];
+console.log(ab)
+ab.push('sdf');
+console.log(ab);
+
+let jakhusi: (string | number)[] = [];
+jakhusi.push('ter');
+console.log(jakhusi)
+let arekta: string | boolean;
+
+
+let bb: object;
+bb = {
+  na: "ashiq",
+  kam: "thinking",
+
+}
+
+console.log(bb);
+
+let mm: {
+  namd: string,
+  boyosd: number,
+  kamd: string,
+} = {};
+
+mm.boyosd = 43;
+mm.namd = "ashiq";
+
+console.log(mm);
+
+let h: {
+  name: string,
+  age: number,
+} = {};
+h.name = "dslkf";
+
+let u: any;
+u = 45;
+u = "sdlfj";
+
+let rt: any[] = [];
+
+rt.push("sdflk");
+rt.push(45);
+console.log(rt);
+
+const myF = () => {
+  console.log("first");
+}
+
+let my: Function;
+my = 5;
+
+const myFuc = (a: string, b: number) => {
+  console.log(a, b);
+}
+
+myFuc("hello", 56);
+
+const hidfuc = (a: string, b: string, c?: number){
+  c? console.log(c): null;
+
+  console.log(a + b);
+  return a + b + c;
+}
+
+hidfuc("helo", "hi", 67);
+hidfuc("helo", "hi");
+
+function ff(a: string, b: string): number{
+
+
+  return 5;
+}
+
+console.log("first");
+
+
+const userDetails = (
+  id: string | number,
+  user: {
+    name: string,
+    age: number
+  }
+
+) => {
+  return id;
+}
+
+
+
+// using type Aliases
+type strOrNum = string | number;
+type userType = {
+  id: strOrNum;
+  name: string;
+  age: number;
+};
+
+const uDetails = (user: userType){
+  console.log(`Hello ${user.name}, ${user.age > 40 ? "sir" : "mr "}`);
+}
+
+const user1 = {
+  id: 1,
+  name: 'Ashiq',
+  age: 24
+}
+
+console.log(uDetails(user1));
